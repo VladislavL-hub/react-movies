0d355eeefe91eef25a1a167c853fc74807ca9aa4
Vladislav Lepekha <vladislavlepekha@gmail.com>
2021-09-16 18:38:35 +0300

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <nav className="#e040fb purple accent-2">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">
+                    Logo
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <a href="!#">Reposit</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };
